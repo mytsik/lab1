8e52011cfea035f839118771225b102cc71ac3c3
mytsik <mycikyanina@gmail.com>
2024-03-04 17:46:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/laboratornaya1/InternetActivity.java
@@ -0,0 +1,48 @@
+package com.example.laboratornaya1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class InternetActivity extends AppCompatActivity {
+    WebView webView;
+
+    final String TAG = "InternetActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internet);
+
+        webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        SimpleWebViewClient webViewClient = new SimpleWebViewClient(this);
+        webView.setWebViewClient(webViewClient);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String link = extras.getString("link");
+            try {
+                webView.loadUrl(link);
+            } catch (Exception e) {
+                Log.e(TAG, Objects.requireNonNull(e.getMessage()));
+                Toast.makeText(this, "Произошла ошибка", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/com/example/laboratornaya1/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.laboratornaya1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    Button makePhoto, openWebsite, makeCall, sendMessage;
+    EditText number, link, message;
+    static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "Find edittext elements by id");
+        number = findViewById(R.id.phone_number);
+        link = findViewById(R.id.link);
+
+        makeCall = findViewById(R.id.make_call);
+        makeCall.setOnClickListener(v -> {
+            String phone_number = number.getText().toString();
+            Intent phone_intent = new Intent(Intent.ACTION_DIAL);
+            phone_intent.setData(Uri.parse("tel:" + phone_number));
+            startActivity(phone_intent);
+        });
+
+        openWebsite = findViewById(R.id.open_website);
+        openWebsite.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, InternetActivity.class);
+            i.putExtra("link", link.getText().toString());
+            startActivity(i);
+        });
+
+        Log.d(TAG, "Set textChangedListener to number edittext");
+        number.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                makeCall.setEnabled(!s.equals(""));
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        link.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                openWebsite.setEnabled(!s.equals(""));
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        makePhoto = findViewById(R.id.make_photo);
+        makePhoto.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, CameraActivity.class);
+            startActivity(i);
+        });
+
+        message = findViewById(R.id.message);
+        sendMessage = findViewById(R.id.send_message);
+        message.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                sendMessage.setEnabled(!s.equals(""));
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        sendMessage.setOnClickListener(v -> {
+            String appName = "org.telegram.messenger";
+            Intent myIntent = new Intent(Intent.ACTION_SEND);
+            myIntent.setType("text/plain");
+            myIntent.setPackage(appName);
+            myIntent.putExtra(Intent.EXTRA_TEXT, message.getText().toString());//
+            startActivity(Intent.createChooser(myIntent, "Share with"));
+        });
+    }
+
+    private static boolean isAppAvailable(Context context, String appName) {
+        PackageManager pm = context.getPackageManager();
+        Log.w(TAG, "Method getInstalledPackages may cause an exception on Android 11");
+        List<PackageInfo> packages = pm.getInstalledPackages(PackageManager.GET_META_DATA);
+        for (PackageInfo info : packages) {
+            if (Objects.equals(info.packageName, appName))
+                return true;
+        }
+        return false;
+    }
+}
